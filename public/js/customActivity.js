5a1a6f6468a00c34d0c6077275a5dc4ef2997b4c
shivshankar0384 <81876897+shivshankar0384@users.noreply.github.com>
2021-04-14 17:59:55 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -56,9 +56,7 @@ define(function (require) {
 	[{
                 "status": "{{Contact.Attribute.APIData.status}}"
         }];
-       console.log(
-                        `Journey saved with payload ${JSON.stringify(payload)}`
-                    )  
+       //console.log( `Journey saved with payload ${JSON.stringify(payload)}`)  
         payload['metaData'].isConfigured = true;
 	connection.trigger('updateActivity', payload);
         
