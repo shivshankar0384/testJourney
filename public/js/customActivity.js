f382b99e126c2891ddfea26a46860313582e3c2a
shivshankar0384 <81876897+shivshankar0384@users.noreply.github.com>
2021-04-14 20:59:39 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -54,7 +54,7 @@ define(function (require) {
 	payload['arguments'].execute = payload['arguments'].execute || {};
        payload['arguments'].execute.inArguments =
 	[{
-             emailAddress: "{{InteractionDefaults.Email}}"
+             "emailAddress": "{{InteractionDefaults.Email}}"
 
         }];
        //console.log( `Journey saved with payload ${JSON.stringify(payload)}`)  
